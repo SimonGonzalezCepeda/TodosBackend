5400a21fa3a83a53c1e09fb32cd10eef035cfed6
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-10-21 16:51:50 +0200

    Created Controller and Task Api (Forgot to call Controller preset)

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+
 class TasksController extends Controller
 {
-    //
+    public function index()
+    {
+        return Task::all();
+    }
 }
---
+++ tests/TaskApiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TaskApiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testShowAllTasks()
+    {
+    $this->json('GET', 'api/task')
+        ->dump()
+        ->seeJson();
+    }
+}
